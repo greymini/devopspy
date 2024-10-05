4289f952549f3ab9d99846e2442990cb352fa8b2
Singh Ayush <79689279+ayushksingh28@users.noreply.github.com>
2024-10-06 01:58:32 +0530

    adding practice files

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+
+#Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Define the host and port to connect to
+host = '127.0.0.1'
+port = 12345
+
+
+#Connect to the server
+client_socket.connect((host, port))
+
+#Send a message to the server
+message = "Hello World!"
+client_socket.send(message.encode("utf-8"))
+
+
+#Receive data from the server
+data = client_socket.recv(1024).decode('utf-8')
+print(f"Received from server: {data}")
+
+#Close the socket
+client_socket.close()
