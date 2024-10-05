2464e67c53f100d5939a0012842cf641fe2e885b
Singh Ayush <79689279+ayushksingh28@users.noreply.github.com>
2024-10-06 02:06:45 +0530

    modifying server.py

--- server.py
+++ server.py
@@ -1,1 +1,32 @@
 import socket
+
+#Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Define the host and port to connect to
+host = '127.0.0.1'
+port = 12345
+
+
+#Connect to the server
+server_socket.connect((host, port))
+
+#Start listening to incoming connections
+server_socket.listen(5)
+
+print(f"Server listening on {host}:{port}....")
+
+#Accept the connection and get the client socket
+client_socket, client_address = server.socket.accept()
+print(f'Connection established with {client_address}')
+
+#Receive data from the client
+data = client_socket.recv(1024).decode('utf-8')
+print(f"Received: {data}")
+
+#Echo the data received back to the client
+client_socket.send(data.encode('utf-8'))
+
+#Close the sockets
+client_socket.close()
+server_socket.close()
